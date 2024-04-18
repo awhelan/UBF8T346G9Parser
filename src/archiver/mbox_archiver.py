5fbf6824c45e990d745441dc59fe3e97acb99f9f
Andy Whelan <andrew.whelan@disney.com>
2024-04-18 09:25:45 -0700

    Cut anything html related out of mbox_archiver

--- src/archiver/mbox_archiver.py
+++ src/archiver/mbox_archiver.py
@@ -20,14 +20,11 @@ class MboxMailArchiver(config_logger.Logger):
     def __init__(self) -> None:
         self.mailsdir = 'MailArchive/Mails'
         self.attachmentsdir = 'MailArchive/Attachments/'
-        self._check_mailsdir(self.mailsdir)
         self.save_paths = {}
 
     def archive_mail(self, mail: Dict, message: bytes) -> None:
-        mail_times = self._get_date(mail)
-        mail_path = self._get_mail_path(mail, mail_times)
-        self._add_save_path(mail, mail_times)
-        self._write_styled_mail(mail, mail_path, message)
+        mbox_path = self.mailsdir + '/inbox.mbox'
+        self._write_styled_mail(mail, mbox_path, message)
 
     def archive_attachment(self, attachment_content: bytes, attachment_name: str) -> None:
         pathlib.Path(self.attachmentsdir).mkdir(parents=True, exist_ok=True)
@@ -41,128 +38,25 @@ class MboxMailArchiver(config_logger.Logger):
             except NotADirectoryError:
                 logging.warning(f'Not a directory')
 
-    def _add_save_path(self, mail, mail_times):
-        """ Add the date, subject, and id of this particular mail to a nested
-         array that tracks all mails per day (for organizing the ui) """
-        year = mail_times.get('year')
-        month = mail_times.get('month')
-        day = mail_times.get('day')
-        mailid = mail.get('id')
+    def _write_styled_mail(self, mail: Dict, mbox_file_path: str, message: bytes):
 
-        mail_info = {
-            'path': 'Mails/{year}/{month}/{day}/{mailid}.html'.format(
-                year=year,
-                month=month,
-                day=day,
-                mailid=mailid
-            ),
-            'subject': mail.get('subject'),
-            'id': mailid
-        }
-
-        if not self.save_paths.get(year):
-            self.save_paths[year] = {}
-
-        if not self.save_paths[year].get(month):
-            self.save_paths[year][month] = {}
-
-        if not self.save_paths[year][month].get(day):
-            self.save_paths[year][month][day] = [mail_info]
-        elif mail_info not in self.save_paths[year][month][day]:
-            self.save_paths[year][month][day].append(mail_info)
-
-    def _write_styled_mail(self, mail: Dict, mail_path: str, message: bytes):
-
-        with helpers.Helper.open_file(mail_path) as f:
+        with helpers.Helper.open_file(mbox_file_path) as f:
             f.write('<head>\n<link rel="stylesheet" href="../../../../static/css/custom.css">\n</head>\n')
             self._write_meta_data(mail, f)
             f.write('\n<div class="bordermail">\n')
             f.write(message.decode('utf-16-le','ignore'))
             f.write('\n</div>\n')
 
+    def _parse_email_address(self, address: Dict) -> str:
+        if address is None:
+            return ''
+        return f"{address.get('email')} {address.get('name')}"
+
     def _write_meta_data(self, mail: Dict, mail_file):
-        meta_data = '''\n<div class="bordermeta">\n<p>\nSubject: <b>{subject}</b>\n</p>\n<p>\nFrom: {sender}\n</p>\n<p>\nTo: {recipients}\n</p>\n<p>\nCC: {cc}\n</p>\n<p>\nDate: {date}\n</p>\n</div>\n'''
-        sendertext = self._get_sender(mail)
-        recipientstext = self._get_recipients(mail)
-        timetext = self._get_time(mail)
-
-        meta_data = meta_data.format(
-            subject=mail.get('subject'),
-            sender=sendertext,
-            recipients=' | '.join(recipientstext),
-            cc=mail.get('cc'),
-            date=timetext
-        )
-
-        mail_file.write(meta_data)
-
-    def _get_time(self, mail):
-        time = self._get_date(mail)
-        timetext = '{hours}:{minutes}:{seconds} {day}/{month}/{year}'.format(
-            year=time.get('year'),
-            month=time.get('month'),
-            day=time.get('day'),
-            hours=time.get('hours'),
-            minutes=time.get('minutes'),
-            seconds=time.get('seconds'),
-        )
-        return timetext
-
-    def _get_sender(self, mail):
         sender = mail.get('sender')
-        sendertext = '<b>{name}</b> - <b>{email}</b>'.format(
-            email=sender.get('email'), #type: ignore
-            name=sender.get('name') #type: ignore
-        )
-        return sendertext
-
-    def _get_recipients(self, mail):
+        sender_text = self._parse_email_address(sender)
         recipients = mail.get('recipients')
-        recipientstext = []
-        if recipients != [None]:
-            for recipient in recipients: #type: ignore
-                recipientstext.append('<b>{name}</b> - <b>{email}</b>'.format(
-                    email=recipient.get('email'), #type: ignore
-                    name=recipient.get('name') #type: ignore
-                ))
-        return recipientstext
-
-    def _get_date(self, mail: Dict) -> Dict:
-        mail_date = datetime.fromtimestamp(mail.get('time'))
-        return {
-            'year': mail_date.strftime("%Y"),
-            'month': mail_date.strftime("%m"),
-            'day': mail_date.strftime("%d"),
-            'hours': mail_date.strftime("%H"),
-            'minutes': mail_date.strftime("%M"),
-            'seconds': mail_date.strftime("%S"),
-        }
-
-    def _get_mail_path(self, mail: Dict, mail_times: Dict) -> str:
-        mail_dir = self._create_mail_dirs(mail_times)
-        return '{}/{}.html'.format(mail_dir, mail.get('id'))
-
-    def _create_mail_dirs(self, mail_times: Dict) -> str:
-        mail_dir = '{mailsdir}/{year}/{month}/{day}'.format(
-            mailsdir=self.mailsdir,
-            year=mail_times.get('year'),
-            month=mail_times.get('month'),
-            day=mail_times.get('day')
-        )
-
-        pathlib.Path(mail_dir).mkdir(parents=True, exist_ok=True)
-        return mail_dir
-
-    def _check_mailsdir(self, mailsdir: str):
-        mails_dir = pathlib.Path(mailsdir)
-        if mails_dir.is_dir():
-            userinput = input('The Mails directory already exists, do you want '
-                              'to start over? [Y/n]: ')
-
-            if 'n' == userinput.lower() or 'no' == userinput.lower():
-                self.logger.info('Exiting.')
-                exit(0)
-            else:
-                self.logger.info(f'Removing {mails_dir}')
-                shutil.rmtree(mails_dir)
-
+        recipients_text = []
+        for recipient in recipients:
+            recipients_text.append(self._parse_email_address(recipient))
+        time = datetime.fromtimestamp(mail.get('time'))
